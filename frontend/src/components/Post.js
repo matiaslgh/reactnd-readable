d2efe63adc7e4981aa20a9cfd0529a73144e8662
matiaslgh <matiaslgh@gmail.com>
2017-12-13 21:24:24 -0300

    feat: material-ui for posts - They are cards now

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import prettyDate from 'pretty-date'
+import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card'
+import { IconButton, Typography } from 'material-ui'
+import { ArrowDropUp, ArrowDropDown } from 'material-ui-icons'
 
 const Post = props => {
   const { post } = props
   return (
-    <div>
-      {post.title}
-    </div>
+    <Card className="postContainer" >
+          <CardHeader
+            // action={
+            //   <IconButton>
+            //     <MoreVertIcon />
+            //   </IconButton>
+            // }
+            title={post.title}
+            subheader={prettyDate.format(new Date(post.timestamp))}
+          />
+          <CardContent>
+            <Typography component="p">
+              {post.body}
+            </Typography>
+          </CardContent>
+          <CardActions disableActionSpacing>
+            <IconButton aria-label="Vote positive">
+              <ArrowDropUp />
+            </IconButton>
+            {post.voteScore}
+            <IconButton aria-label="Vote negative">
+              <ArrowDropDown />
+            </IconButton>
+          </CardActions>
+        </Card>
   )
 }
 
